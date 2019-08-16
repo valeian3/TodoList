d3917ef76180a925810cdea1d964bdbb95a46080
vBahnik <valerian.bahnik@hotmail.com>
2019-08-16 21:55:13 +0200

    added new activity

--- app/src/main/java/com/example/todolist/MainActivity.java
+++ app/src/main/java/com/example/todolist/MainActivity.java
@@ -1,13 +1,39 @@
 package com.example.todolist;
 
+import android.content.Intent;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    FloatingActionButton addNewTaskBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        setUpUi();
     }
+
+    private void setUpUi(){
+        addNewTaskBtn = findViewById(R.id.fab);
+        addNewTaskBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent explicitIntent = new Intent();
+                explicitIntent.setClass(getApplicationContext(), NewTask.class);
+                startActivity(explicitIntent);
+                displayToast("NewTask Activity opened.");
+            }
+        });
+    }
+
+    private void displayToast(String message){
+        Toast.makeText(this,message, Toast.LENGTH_SHORT).show();
+    }
+
 }
